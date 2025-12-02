23f30c206fa6ed6fc0b23fc48445684678c22c63
spivakkrytoi <sasha.spivak2013@gmail.com>
2025-12-02 22:00:25 +0200

    added more fonts to ascii generator, and fixed some bugs

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from rich.progress import Progress
 import rainbowtext
 from datetime import datetime
 
-#mine "libs" :)
 from libs import calc
 from libs import config
 from libs import settings
 from libs.welcome import print_ascii_slow
 
-month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
+         "December"]
 
 colorama.init()
 os.system("clear")
@@ -28,6 +28,78 @@ with Progress() as progress:
 os.system("clear")
 
 print_ascii_slow("Welcome   to   SL!", delay=0.005)
+
+def texttoart():
+    listOfFonts = '''
+    1. Small
+    2. Medium
+    3. Large
+    '''
+    print(listOfFonts)
+    
+    try:
+        font = int(input("What font do you need? (Number of font): "))
+    except ValueError:
+        print("Please enter a valid number!")
+        return
+    
+    atts = 5
+    
+    for i in range(atts):
+        print(f"\n--- Attempt {i+1}/{atts} ---")
+        
+        send_to_art = input("Text: ")
+        
+        if font == 1:
+            art_text = text2art(send_to_art, font='small')
+        elif font == 2:
+            art_text = text2art(send_to_art, font='medium')
+        elif font == 3:
+            art_text = text2art(send_to_art, font='large')
+        else:
+            print("Incorrect number of font! Please choose 1, 2, or 3.")
+            try:
+                font = int(input("What font do you need? (Number of font): "))
+                continue
+            except ValueError:
+                print("Invalid input. Ending program.")
+                break
+        
+        print(f"You have: {atts - i - 1} attempts remaining")
+        print(art_text)
+    
+    print("\nBuy some new atts at: example.com")
+
+
+def calculator():
+    calc.option()
+    try:
+        action = int(input("Enter option: "))
+    except ValueError:
+        return
+
+    if action == 1:
+        os.system("clear")
+        x = float(input("Enter number 1: "))
+        y = float(input("Enter number 2: "))
+        calc.add(x, y)
+    elif action == 2:
+        os.system("clear")
+        x = float(input("Enter number 1: "))
+        y = float(input("Enter number 2: "))
+        calc.minus(x, y)
+    elif action == 3:
+        os.system("clear")
+        x = float(input("Enter number 1: "))
+        y = float(input("Enter number 2: "))
+        calc.multiply(x, y)
+    elif action == 4:
+        os.system("clear")
+        x = float(input("Enter number 1: "))
+        y = float(input("Enter number 2: "))
+        calc.division(x, y)
+
+
 while True:
     options = rainbowtext.text("""
     _____________________________
@@ -38,7 +110,8 @@ while True:
     -----------------------------
     """)
     month_now = int(datetime.now().strftime("%m")) - 1
-    now = "~Time:" + datetime.now().strftime("%d") + " " +  month[month_now] + " " + datetime.now().strftime("%H:%M" + "~")
+    now = "~Time:" + datetime.now().strftime("%d") + " " + month[month_now] + " " + datetime.now().strftime(
+        "%H:%M" + "~")
     print("    " + Fore.RED + now)
     print(options)
 
@@ -49,49 +122,9 @@ while True:
         os.system("clear")
         continue
 
-    def texttoart(text):
-        text = text2art(text, font='small')
-        print(text)
-
-    def calculator():
-        calc.option()
-        try:
-            action = int(input("Enter option: "))
-        except ValueError:
-            return
-
-        if action == 1:
-            os.system("clear")
-            x = float(input("Enter number 1: "))
-            y = float(input("Enter number 2: "))
-            calc.add(x, y)
-        elif action == 2:
-            os.system("clear")
-            x = float(input("Enter number 1: "))
-            y = float(input("Enter number 2: "))
-            calc.minus(x, y)
-        elif action == 3:
-            os.system("clear")
-            x = float(input("Enter number 1: "))
-            y = float(input("Enter number 2: "))
-            calc.multiply(x, y)
-        elif action == 4:
-            os.system("clear")
-            x = float(input("Enter number 1: "))
-            y = float(input("Enter number 2: "))
-            calc.division(x, y)
-
-    def texttoartfnc():
-        atts = 5
-        for i in range(atts):
-            print("You have:", atts - i, "attempts :)")
-            send_to_art = input("Text: ")
-            texttoart(send_to_art)
-        print("Buy some new atts at: example.com")
-
     if choose == 1:
         os.system("clear")
-        texttoartfnc()
+        texttoart()
     elif choose == 2:
         os.system("clear")
         calculator()
@@ -110,19 +143,15 @@ while True:
             appquanty = int(input("How many programs you need to add to your config?"))
             config.createcfg(appquanty)
         elif cfginput == 2:
-                    config.menu()
-                    cfgToLoad = input("What config you need to load? (just name after /Configs/): ")
-                    config.loadcfg(cfgToLoad)
+            config.menu()
+            cfgToLoad = input("What config you need to load? (just name after /Configs/): ")
+            config.loadcfg(cfgToLoad)
         elif cfginput == 3:
-                os.system("clear")
-        else:
-            print("Incorrect! Try again")
-
-
+            os.system("clear")
     elif choose == 99:
         os.system("clear")
         print("Goodbye!")
         sys.exit(0)
     else:
         print("Incorrect number!")
-    os.system("clear")
+        os.system("clear")
